ea995ec3c733c124372bbf2e48046858a5e11921
p3trws <peu.bertoldo@gmail.com>
2025-04-17 19:15:33 -0300

    home add e algumas alterações no html e css do poertifólio original

--- Portifólio/js/index.js
+++
@@ -1,6 +0,0 @@
-function scrollProjetos(direcao) {
-    const container = document.getElementById("projetosGrid");
-    const card = container.querySelector(".projeto-card");
-    const scrollAmount = card.offsetWidth + 16; // 16 é o gap
-    container.scrollLeft += direcao * scrollAmount;
-}
